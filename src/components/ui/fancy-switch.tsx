5f5d96aad70cf852be21643e6a54cc74dd30357b
Dominik Hodan <domho108@gmail.com>
2024-11-12 10:31:02 +0100

    disable fancy switch animation on initial load

--- src/components/ui/fancy-switch.tsx
+++ src/components/ui/fancy-switch.tsx
@@ -1,6 +1,12 @@
 
 import { FancySwitch as FancySwitchBase } from '@omit/react-fancy-switch'
+import { useEffect, useState } from 'react';
 export default function FancySwitch({ options, selectedOption, setSelectedOption, roundedClass, children, vertical = false, full = false }) {
+    const [animated, setAnimated] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => setAnimated(true), 100)
+        return () => clearTimeout(timer)
+    }, []);
     return (
         <div className={"h-full bg-white dark:bg-background/90 font-bold rounded-full transition-all shadow-sm flex outline-primary dark:outline-primary/30 outline-2 outline select-none " + (full ? "w-full justify-center" : "w-fit")}>
             <FancySwitchBase
@@ -8,9 +14,10 @@ export default function FancySwitch({ options, selectedOption, setSelectedOption
                 value={selectedOption}
                 onChange={setSelectedOption}
                 className={"flex w-fit h-full text-sm text-primary " + (children ? " pr-1 " : "") + (vertical ? " flex-col py-2" : "")}
-                highlighterClassName={"bg-primary h-full " + roundedClass}
+                highlighterClassName={"transition-none bg-primary h-full " + roundedClass}
                 radioClassName="relative mx-2 flex h-full cursor-pointer items-center justify-center rounded-full px-3.5 text-sm text-primary-foreground transition-colors focus:outline-none data-[checked]:text-primary-foreground"
                 highlighterIncludeMargin={true}
+                highlighterStyle={{ transitionProperty: animated ? "all" : "none" }}
                 renderOption={({ option, isSelected, getOptionProps }) => (
                     <div {...getOptionProps()}
                         className={"flex items-center gap-2 z-50 justify-center " + (vertical ? " px-3" : " px-4 ") + (isSelected ? " text-primary-foreground" : "") + (option == options.slice(-1)[0] ? "rounded-full" : "")}>
