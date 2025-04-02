a9c83725839699e12212ef458e1f63fea89bee09
Dominik Hodan <domho108@gmail.com>
2025-04-02 16:05:04 +0200

    avoid vignetting on background image

--- src/routes/SongView/components/BackgroundImage.tsx
+++ src/routes/SongView/components/BackgroundImage.tsx
@@ -5,22 +5,25 @@ import { SongData } from "@/types/types";
 interface BackgroundImageProps {
   songData: SongData;
   id: string;
-  className?: string; 
+  className?: string;
 }
 
 const BackgroundImage: React.FC<BackgroundImageProps> = ({ songData, id, className }) => {
+  // the image has to be slightly larger than necessary to avoid vignetting - that is the reason for the outer wrapper
   return (
-    <div
-      className={cn(
-        "absolute top-0 left-0 min-h-lvh h-full w-full bg-image -z-20 blur-lg overflow-hidden transition-all duration-500 ease-in-out",
-        className
-      )}
-      id={id}
-      style={{ backgroundImage: `url(${songData.thumbnailURL()})` }}
-    >
-      <div className="w-full h-full
-       bg-[#fdefe2]/75
+    <div className="absolute top-0 left-0 h-full w-full overflow-clip  -z-20">
+      <div
+        className={cn(
+          "absolute -top-[5%] -left-[5%] h-[110%] w-[110%] bg-image blur-lg overflow-hidden transition-all duration-500 ease-in-out",
+          className
+        )}
+        id={id}
+        style={{ backgroundImage: `url(${songData.thumbnailURL()})` }}
+      >
+        <div className="w-full h-full
+       bg-background/65
        dark:bg-glass/50"></div>
+      </div>
     </div>
   );
 };
