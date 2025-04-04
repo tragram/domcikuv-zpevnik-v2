f4ca3df1e870484b6468d34299499f96ad43f30a
Dominik Hodan <domho108@gmail.com>
2025-04-04 15:19:57 +0200

    fix svg/react issues

--- src/routes/SongView/settings/smart_columns_icon.tsx
+++ src/routes/SongView/settings/smart_columns_icon.tsx
@@ -1,13 +1,12 @@
-import smartIcon from "./smart-columns.svg"
 const SmartColumnsIcon = () => {
     return (
         <svg width="24" height="24" version="1.1" viewBox="0 0 6.35 6.35" xmlns="http://www.w3.org/2000/svg">
-            <g transform="translate(-102.39 -133.09)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-                <g transform="matrix(.29104 0 0 .29104 102.08 132.77)" stroke-width="2">
+            <g transform="translate(-102.39 -133.09)" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                <g transform="matrix(.29104 0 0 .29104 102.08 132.77)" strokeWidth="2">
                     <rect x="3" y="3" width="18" height="18" rx="2" />
-                    <path d="m12 3v18" stroke-width="1.5118" />
+                    <path d="m12 3v18" strokeWidth="1.5118" />
                 </g>
-                <g transform="matrix(.17359 0 0 .17359 103.49 134.18)" stroke-width="2.575">
+                <g transform="matrix(.17359 0 0 .17359 103.49 134.18)" strokeWidth="2.575">
                     <path d="m12 5a3 3 0 1 0-5.997 0.125 4 4 0 0 0-2.526 5.77 4 4 0 0 0 0.556 6.588 4 4 0 1 0 7.967 0.517z" />
                     <path d="m12 5a3 3 0 1 1 5.997 0.125 4 4 0 0 1 2.526 5.77 4 4 0 0 1-0.556 6.588 4 4 0 1 1-7.967 0.517z" />
                     <path d="m15 13a4.5 4.5 0 0 1-3-4 4.5 4.5 0 0 1-3 4" />
