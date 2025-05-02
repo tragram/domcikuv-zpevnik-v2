2ece6e50b00e8f5717559c08276e96d65ed75f93
Dominik Hodan <domho108@gmail.com>
2025-05-02 23:48:34 +0200

    basic ban list of random songs

--- src/components/RandomSong.tsx
+++ src/components/RandomSong.tsx
@@ -2,15 +2,32 @@ import { Dices } from 'lucide-react';
 import React from 'react';
 import { Button } from './ui/button';
 import { useNavigate } from 'react-router-dom';
-//TODO: select should have a default value
-export function randomSongURL(songs) {
-    const selectedSong = songs[Math.floor(Math.random() * songs.length)];
-    return selectedSong.url();
+import useLocalStorageState from 'use-local-storage-state';
+import { SongData } from '@/types/types';
+
+function randomSong(songs: SongData[], bannedSongs: string[]): SongData {
+    console.log(bannedSongs)
+    if (songs.length === bannedSongs.length) {
+        throw Error("No more random songs available!")
+    }
+    while (true) {
+        const selectedSong = songs[Math.floor(Math.random() * songs.length)];
+        if (!bannedSongs.includes(selectedSong.id)) {
+            return selectedSong;
+        }
+        console.log("tried to open",selectedSong.id,"but did not!")
+    }
 }
 function RandomSong({ songs }) {
     const navigate = useNavigate();
+    const [bannedSongs, setBannedSongs] = useLocalStorageState<string[]>("songsBannedFromRandom", { defaultValue: [] });
+    const selectSong = () => {
+        const chosenSong = randomSong(songs, bannedSongs);
+        setBannedSongs([...bannedSongs, chosenSong.id]);
+        return chosenSong;
+    }
     return (
-        <Button size="icon" variant="circular" onClick={() => navigate(randomSongURL(songs))}>
+        <Button size="icon" variant="circular" onClick={() => navigate(selectSong().url())}>
             <Dices />
         </Button>
     )
