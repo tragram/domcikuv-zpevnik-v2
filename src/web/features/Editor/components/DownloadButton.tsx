005bde347998dd8eb3787f81e20feb7fdbf9fde8
Dominik Hodan <domho108@gmail.com>
2025-07-16 22:41:20 +0200

    listen for ctrl+s in editor

--- src/web/features/Editor/components/DownloadButton.tsx
+++ src/web/features/Editor/components/DownloadButton.tsx
@@ -2,6 +2,7 @@ import { Button } from "~/components/ui/button";
 import { type SongMetadata } from "~/types/songData";
 import { Download } from "lucide-react";
 import { editorToChordPro } from "./utils";
+import { useEffect, useCallback } from "react";
 
 type DownloadButtonProps = {
   metadata: SongMetadata;
@@ -13,7 +14,7 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
   content,
 }) => {
   // Function to handle the download
-  const handleDownload = () => {
+  const handleDownload = useCallback(() => {
     const { filename, chordProContent } = editorToChordPro(metadata, content);
     // Create a blob and download it
     const blob = new Blob([chordProContent], { type: "text/plain" });
@@ -30,7 +31,27 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
       document.body.removeChild(a);
       URL.revokeObjectURL(url);
     }, 0);
-  };
+  }, [metadata, content]);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+        // Prevent the Save dialog to open
+        e.preventDefault();
+        // download
+        handleDownload();
+      }
+    };
+
+    // Add event listener
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Cleanup function to remove event listener
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleDownload]);
+
   return (
     <Button onClick={handleDownload} variant="default">
       Download ChordPro
