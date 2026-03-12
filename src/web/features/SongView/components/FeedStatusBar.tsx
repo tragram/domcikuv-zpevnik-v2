eae63d403925b429ddc1f6062abbf0bd4077892e
Dominik Hodan <domho108@gmail.com>
2026-03-12 18:32:25 +0100

    Update FeedStatusBar to show correct info on master takeover

--- src/web/features/SongView/components/FeedStatusBar.tsx
+++ src/web/features/SongView/components/FeedStatusBar.tsx
@@ -1,3 +1,4 @@
+import { useIsOnline } from "~/hooks/use-is-online";
 import { FeedStatus } from "../SongView";
 
 interface FeedStatusBarProps {
@@ -5,10 +6,20 @@ interface FeedStatusBarProps {
 }
 
 export const FeedStatusBar = ({ feedStatus }: FeedStatusBarProps) => {
+  const isOnline = useIsOnline();
   if (!feedStatus?.enabled) return null;
-  const masterStatus = feedStatus.isConnected
-    ? `Sharing session · ${feedStatus.connectedClients} connected`
-    : "Reconnect to internet to keep sharing your session";
+  const masterStatus = isOnline ? (
+    feedStatus.isConnected ? (
+      `Sharing session · ${feedStatus.connectedClients} connected`
+    ) : (
+      <>
+        Your session was taken over by another device. <wbr />
+        Reload to restore control.
+      </>
+    )
+  ) : (
+    "Reconnect to internet to keep sharing your session"
+  );
 
   const followerStatus = feedStatus.isConnected
     ? `Following ${feedStatus.sessionState?.masterNickname}'s feed`
@@ -16,7 +27,7 @@ export const FeedStatusBar = ({ feedStatus }: FeedStatusBarProps) => {
 
   const statusMessage = feedStatus?.isMaster ? masterStatus : followerStatus;
   return (
-    <div className="fixed bottom-0 left-0 right-0 h-8 w-full flex justify-center items-center text-primary bg-glass/30 dark:bg-glass/90 dark:text-white/70 text-xs backdrop-blur-md border-primary dark:border-primary/30 border-t-2 z-50">
+    <div className="fixed bottom-0 left-0 right-0 h-fit w-full py-2 px-4 text-center flex justify-center items-center text-primary bg-glass/30 dark:bg-glass/90 dark:text-white/70 text-xs backdrop-blur-md border-primary dark:border-primary/30 border-t-2 z-50 whitespace-nowrap">
       {statusMessage}
     </div>
   );
