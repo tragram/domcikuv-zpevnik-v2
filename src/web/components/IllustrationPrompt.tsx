7d00d96fc0cd5b26ca4bed477e4336d52a9cbc52
Dominik Hodan <domho108@gmail.com>
2026-04-06 11:31:51 +0200

    refactor IllustrationPrompt: simplify loading and error handling logic for better readability

--- src/web/components/IllustrationPrompt.tsx
+++ src/web/components/IllustrationPrompt.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { AutoTextSize } from "auto-text-size";
 import { SongsAPI } from "src/worker/api-client";
+import { useIsOnline } from "~/hooks/use-is-online";
 import { cn } from "~/lib/utils";
 import { fetchIllustrationPrompt } from "~/services/illustration-service";
 import { SongData } from "~/types/songData";
@@ -29,23 +30,23 @@ export function IllustrationPrompt({
     retry: 1,
     staleTime: 5 * 60 * 1000,
   });
-
+  const isOnline = useIsOnline();
   if (!show) {
     return null;
   }
 
+  let text = promptContent ?? "";
+  if (!isOnline && !promptContent) {
+    text =
+      "You must be online to see full resolution illustrations and their prompts.";
+  } else if (error) {
+    text = "No description available";
+  }
+
   return (
     <div className={cn("px-4 flex flex-grow my-4 w-full", className)}>
-      <AutoTextSize mode="boxoneline" maxFontSizePx={60}>
-        {isLoading ? (
-          <p className="text-wrap w-full text-shadow opacity-50"></p>
-        ) : error ? (
-          <p className="text-wrap w-full text-shadow opacity-50">
-            No description available
-          </p>
-        ) : (
-          <p className="text-wrap w-full text-shadow">{promptContent || ""}</p>
-        )}
+      <AutoTextSize mode="boxoneline" maxFontSizePx={40}>
+        <p className="text-wrap w-full text-shadow">{text}</p>
       </AutoTextSize>
     </div>
   );
